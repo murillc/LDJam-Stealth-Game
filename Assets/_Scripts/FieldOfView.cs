9ddfeebe211fa8b10521f35f1070a67a37ee365f
Carlos Murillo <crmurillo117@gmail.com>
2022-04-02 12:41:43 -0400

    Field of View implementation init

---
+++ Assets/_Scripts/FieldOfView.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 6;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 10f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            vertices[vertexIndex] = vertex;
+
+                if (i > 0) {
+                    triangles[triangleIndex + 0] = 0;
+                    triangles[triangleIndex + 1] = vertexIndex - 1;
+                    triangles[triangleIndex + 2] = vertexIndex;
+
+                    triangleIndex += 3;
+                }
+            vertexIndex++;
+
+
+            angle -= angleIncrease;
+        }
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    private static Vector3 GetVectorFromAngle(float angleDeg)
+    {
+        float angleRad = angleDeg * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
