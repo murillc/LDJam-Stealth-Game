0c24d76d479116b3b8b150fb979c40e031144c05
Ramin Amiri <zeuspsn.55555@gmail.com>
2022-04-02 13:34:46 -0400

    removed error real quick

--- Assets/_Scripts/Pathfinding.cs
+++ Assets/_Scripts/Pathfinding.cs
@@ -14,19 +14,19 @@ public class Pathfinding
         grid = new Grid<PathNode>(parentTransform, width, height, cellSizeX, cellSizeY, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
     }
 
-    private List<PathNode> FindPath(int startX, int startY, int endX, int endY)
-    {
-        PathNode startNode = grid.GetGridObject(startX, startY);
+    //private List<PathNode> FindPath(int startX, int startY, int endX, int endY)
+    //{
+    //    PathNode startNode = grid.GetGridObject(startX, startY);
 
-        openList = new List<PathNode>() {  startNode };
-        closedList = new List<PathNode>();
+    //    openList = new List<PathNode>() {  startNode };
+    //    closedList = new List<PathNode>();
 
-        for (int x = 0; x < grid.GetWidth();  x++)
-            for (int y = 0; y < grid.GetHeight(); y++)
-            {
-                PathNode pathNode = grid.GetGridObject(x, y);
-                pathNode.gCost = int.MaxValue;
-                //pathNode.CalculateFCost();
-            }
-    }
+    //    for (int x = 0; x < grid.GetWidth();  x++)
+    //        for (int y = 0; y < grid.GetHeight(); y++)
+    //        {
+    //            PathNode pathNode = grid.GetGridObject(x, y);
+    //            pathNode.gCost = int.MaxValue;
+    //            //pathNode.CalculateFCost();
+    //        }
+    //}
 }
