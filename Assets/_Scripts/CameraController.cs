485452c87f8bf0223334b09049be026f89b61cb4
Carlos Murillo <crmurillo117@gmail.com>
2022-04-02 12:16:10 -0400

    New tileset + Basic camera control

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x + offset.x, playerTransform.position.y + offset.y, offset.z);
+    }
+}
