6a4ff69d9936e21a8330b85132c42d53a887c86a
Ramin Amiri <zeuspsn.55555@gmail.com>
2022-04-02 04:59:37 -0400

    grid stuff

--- Assets/_Scripts/Grid.cs
+++ Assets/_Scripts/Grid.cs
@@ -1,33 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class Grid<T> : MonoBehaviour
 {
-    private int[,] gridArray;
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
 
     private int width;
     private int height;
 
-    private float cellSize;
+    private float cellSizeX;
+    private float cellSizeY;
 
-    public Grid(int width, int height, float cellSize)
+    private T[,] gridArray;
+
+    private GridManager gridManager;
+
+    public Grid(GridManager gridManager, int width, int height, float cellSizeX, float cellSizeY, Func<Grid<T>, int, int, T> createGridObject)
     {
         this.width = width;
         this.height = height;
-        this.cellSize = cellSize;
+        this.cellSizeX = cellSizeX;
+        this.cellSizeY = cellSizeY;
+        this.gridManager = gridManager;
 
-        gridArray = new int[width, height];
+        gridArray = new T[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
+                GameObject newCell = Instantiate(gridManager.cell, gridManager.transform);
 
+                Transform cellT = newCell.GetComponent<Transform>();
+
+                //Spawn depending on parent position, place in array, and with half width and height offset because the pivot is in the center instead of bottom left, spawning them incorrectly
+                cellT.position = new Vector3(gridManager.transform.position.x + (x * cellSizeX) + cellSizeX / 2, gridManager.transform.position.y + (y * cellSizeY) + cellSizeY / 2, 0);
+
+                TextMeshPro textMesh = newCell.GetComponentInChildren<TextMeshPro>();
+                textMesh.text = "[" + x + ", " + y + "]";
+
+                gridArray[x, y] = createGridObject(this, x, y);
             }
+
+        OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) => 
+        {
+            Debug.Log(eventArgs.x + eventArgs.y);
+        };
     }
 
-    private Vector3 GetWorldPosition(int x, int y)
+    public void TriggerGridObjectChanged(int x, int y)
     {
-        return new Vector3(x, y) * cellSize;
+        if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+    }
+
+    public void SetGridObject(int x, int y, T value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public T GetGridObject(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return default(T);
+        }
     }
 }
--- Assets/_Scripts/GridManager.cs
+++ Assets/_Scripts/GridManager.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
-    Grid grid;
+    private Grid<PathNode> grid;
+
+    public GameObject cell;
+
+    public int width;
+    public int height;
+
+    public float cellSizeX;
+    public float cellSizeY;
 
     void Start()
     {
-        grid = new Grid(20, 20, 16f);
+        grid = new Grid<PathNode>(this, width, height, cellSizeX, cellSizeY, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
     }
+
+    //Grid MakeGrid(int width, int height, float cellSizeX, float cellSizeY, Func<Grid<PathNode>, int, int, PathNode> createGridObject)
+    //{
+    //    return new Grid<PathNode>(this, width, height, 9, 5, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+    //}
 }
