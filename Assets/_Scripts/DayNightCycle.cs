b8cc910ab4230f801af6737bbecc8b2a9f7930c9
Ramin Amiri <zeuspsn.55555@gmail.com>
2022-04-04 07:11:05 -0400

    day night stuff

--- Assets/_Scripts/DayNightCycle.cs
+++ Assets/_Scripts/DayNightCycle.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public enum Cycle
+    public enum CycleEnum
     {
         DAY,
         NIGHT
     }
 
-    public Cycle currentCycle;
+    public CycleEnum currentCycle;
+    private CycleEnum oldCycle;
+
+    public GameObject dayObjects;
+    public GameObject nightObjects;
+    public GameObject playerLight;
 
     private void Awake()
     {
-        currentCycle = Cycle.NIGHT;
+        currentCycle = CycleEnum.NIGHT;
+        oldCycle = currentCycle;
     }
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (currentCycle != oldCycle)
+        {
+            switch (currentCycle)
+            {
+                case CycleEnum.DAY:
+                    nightObjects.SetActive(false);
+                    playerLight.SetActive(false);
+                    dayObjects.SetActive(true);
+                    break;
+
+                case CycleEnum.NIGHT:
+                    nightObjects.SetActive(true);
+                    playerLight.SetActive(true);
+                    dayObjects.SetActive(false);
+                    break;
+
+                default:
+                    Debug.Log("DEFAULT, NO CYCLE??");
+                    break;
+            }
+
+            oldCycle = currentCycle;
+        }
     }
 }
